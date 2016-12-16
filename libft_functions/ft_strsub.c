8fce562bf3718c9a0df4399aae3956d5ba976302
Matthew Vogee <mvogee@yahoo.com>
2016-12-15 16:28:39 -0800

    libft_functions folder

---
+++ libft_functions/ft_strsub.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvogee <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/14 15:05:50 by mvogee            #+#    #+#             */
+/*   Updated: 2016/12/14 15:05:52 by mvogee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../ft_printf.h"
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	count;
+
+	count = 0;
+	if (!(substr = (char*)malloc(sizeof(char) * len + 1)))
+		return (NULL);
+	while (count < len)
+	{
+		substr[count] = s[count + start];
+		count++;
+	}
+	substr[count] = '\0';
+	return (substr);
+}
